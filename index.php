d8a3fbb9a23e71e58d38be083f223315a8f08eac
keitaabdou <abdou.keita@uvci.edu.ci>
2021-12-23 16:04:08 -0900

    envoi des premier fichier sur github

---
+++ index.php
@@ -0,0 +1,273 @@
+<?php include 'includes/header.php'; ?>
+
+        <section class="section first-section">
+            <div class="container-fluid">
+                <div class="masonry-blog clearfix">
+                    <div class="left-side">
+                        <div class="masonry-box post-media">
+                             <img src="upload/garden_cat_01.jpg" alt="" class="img-fluid">
+                             <div class="shadoweffect">
+                                <div class="shadow-desc">
+                                    <div class="blog-meta">
+                                        <span class="bg-aqua"><a href="blog-category-01.html" title="">Gardening</a></span>
+                                        <h4><a href="single.html" title="">How to choose high quality soil for your gardens</a></h4>
+                                        <small><a href="single.html" title="">21 July, 2017</a></small>
+                                        <small><a href="#" title="">by Amanda</a></small>
+                                    </div><!-- end meta -->
+                                </div><!-- end shadow-desc -->
+                            </div><!-- end shadow -->
+                        </div><!-- end post-media -->
+                    </div><!-- end left-side -->
+
+                    <div class="center-side">
+                        <div class="masonry-box post-media">
+                             <img src="upload/garden_cat_02.jpg" alt="" class="img-fluid">
+                             <div class="shadoweffect">
+                                <div class="shadow-desc">
+                                    <div class="blog-meta">
+                                        <span class="bg-aqua"><a href="blog-category-01.html" title="">Outdoor</a></span>
+                                        <h4><a href="single.html" title="">You can create a garden with furniture in your home</a></h4>
+                                        <small><a href="single.html" title="">19 July, 2017</a></small>
+                                        <small><a href="#" title="">by Amanda</a></small>
+                                    </div><!-- end meta -->
+                                </div><!-- end shadow-desc -->
+                            </div><!-- end shadow -->
+                        </div><!-- end post-media -->
+                    </div><!-- end left-side -->
+
+                    <div class="right-side hidden-md-down">
+                        <div class="masonry-box post-media">
+                             <img src="upload/garden_cat_03.jpg" alt="" class="img-fluid">
+                             <div class="shadoweffect">
+                                <div class="shadow-desc">
+                                    <div class="blog-meta">
+                                        <span class="bg-aqua"><a href="blog-category-01.html" title="">Indoor</a></span>
+                                        <h4><a href="single.html" title="">The success of the 10 companies in the vegetable sector</a></h4>
+                                        <small><a href="single.html" title="">03 July, 2017</a></small>
+                                        <small><a href="#" title="">by Jessica</a></small>
+                                    </div><!-- end meta -->
+                                </div><!-- end shadow-desc -->
+                             </div><!-- end shadow -->
+                        </div><!-- end post-media -->
+                    </div><!-- end right-side -->
+                </div><!-- end masonry -->
+            </div>
+        </section>
+
+        <section class="section wb">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-9 col-md-12 col-sm-12 col-xs-12">
+                        <div class="page-wrapper">
+                            <div class="blog-list clearfix">
+                                <div class="blog-box row">
+                                    <div class="col-md-4">
+                                        <div class="post-media">
+                                            <a href="simgle.html" title="">
+                                                <img src="upload/garden_sq_01.jpg" alt="" class="img-fluid">
+                                                <div class="hovereffect"></div>
+                                            </a>
+                                        </div><!-- end media -->
+                                    </div><!-- end col -->
+
+                                    <div class="blog-meta big-meta col-md-8">
+                                        <span class="bg-aqua"><a href="category.html" title="">Indoor</a></span>
+                                        <h4><a href="simgle.html" title="">The best twenty plant species you can look at at home</a></h4>
+                                        <p>Aenean interdum arcu blandit, vehicula magna non, placerat elit. Mauris et pharetratortor. Suspendissea sodales urna. In at augue elit. Vivamus enim nibh, maximus ac felis nec, maximus tempor odio.</p>
+                                        <small><a href="category.html" title=""><i class="fa fa-eye"></i> 1887</a></small>
+                                        <small><a href="simgle.html" title="">11 July, 2017</a></small>
+                                        <small><a href="#" title="">by Matilda</a></small>
+                                    </div><!-- end meta -->
+                                </div><!-- end blog-box -->
+
+                                <hr class="invis">
+
+                                <div class="blog-box row">
+                                    <div class="col-md-4">
+                                        <div class="post-media">
+                                            <a href="simgle.html" title="">
+                                                <img src="upload/garden_sq_02.jpg" alt="" class="img-fluid">
+                                                <div class="hovereffect"></div>
+                                            </a>
+                                        </div><!-- end media -->
+                                    </div><!-- end col -->
+
+                                    <div class="blog-meta big-meta col-md-8">
+                                        <span class="bg-aqua"><a href="category.html" title="">Indoor</a></span>
+                                        <h4><a href="simgle.html" title="">How do you like the idea of creating a garden from the Amazon forests?</a></h4>
+                                        <p>Aenean interdum arcu blandit, vehicula magna non, placerat elit. Mauris et pharetratortor. Suspendissea sodales urna. In at augue elit. Vivamus enim nibh, maximus ac felis nec, maximus tempor odio.</p>
+                                        <small><a href="category.html" title=""><i class="fa fa-eye"></i> 4441</a></small>
+                                        <small><a href="simgle.html" title="">10 July, 2017</a></small>
+                                        <small><a href="#" title="">by Matilda</a></small>
+                                    </div><!-- end meta -->
+                                </div><!-- end blog-box -->
+
+                                <hr class="invis">
+
+                                <div class="blog-box row">
+                                    <div class="col-md-4">
+                                        <div class="post-media">
+                                            <a href="simgle.html" title="">
+                                                <img src="upload/garden_sq_03.jpg" alt="" class="img-fluid">
+                                                <div class="hovereffect"></div>
+                                            </a>
+                                        </div><!-- end media -->
+                                    </div><!-- end col -->
+
+                                    <div class="blog-meta big-meta col-md-8">
+                                        <span class="bg-aqua"><a href="category.html" title="">Indoor</a></span>
+                                        <h4><a href="simgle.html" title="">Top 20 cafeterias decorated with quality plants</a></h4>
+                                        <p>Aenean interdum arcu blandit, vehicula magna non, placerat elit. Mauris et pharetratortor. Suspendissea sodales urna. In at augue elit. Vivamus enim nibh, maximus ac felis nec, maximus tempor odio.</p>
+                                        <small><a href="category.html" title=""><i class="fa fa-eye"></i> 1234</a></small>
+                                        <small><a href="simgle.html" title="">09 July, 2017</a></small>
+                                        <small><a href="#" title="">by Matilda</a></small>
+                                    </div><!-- end meta -->
+                                </div><!-- end blog-box -->
+
+                                <hr class="invis">
+
+                                <div class="row">
+                                    <div class="col-lg-10 offset-lg-1">
+                                        <div class="banner-spot clearfix">
+                                            <div class="banner-img">
+                                                <img src="upload/banner_05.jpg" alt="" class="img-fluid">
+                                            </div><!-- end banner-img -->
+                                        </div><!-- end banner -->
+                                    </div><!-- end col -->
+                                </div><!-- end row -->
+
+                                <hr class="invis">
+
+                                <div class="blog-box row">
+                                    <div class="col-md-4">
+                                        <div class="post-media">
+                                            <a href="simgle.html" title="">
+                                                <img src="upload/garden_sq_04.jpg" alt="" class="img-fluid">
+                                                <div class="hovereffect"></div>
+                                            </a>
+                                        </div><!-- end media -->
+                                    </div><!-- end col -->
+
+                                    <div class="blog-meta big-meta col-md-8">
+                                        <span class="bg-aqua"><a href="category.html" title="">Indoor</a></span>
+                                        <h4><a href="simgle.html" title="">30 beautiful flowers you can look at in offices and workplaces</a></h4>
+                                        <p>Aenean interdum arcu blandit, vehicula magna non, placerat elit. Mauris et pharetratortor. Suspendissea sodales urna. In at augue elit. Vivamus enim nibh, maximus ac felis nec, maximus tempor odio.</p>
+                                        <small><a href="category.html" title=""><i class="fa fa-eye"></i> 4441</a></small>
+                                        <small><a href="simgle.html" title="">08 July, 2017</a></small>
+                                        <small><a href="#" title="">by Matilda</a></small>
+                                    </div><!-- end meta -->
+                                </div><!-- end blog-box -->
+
+                                <hr class="invis">
+
+                                <div class="blog-box row">
+                                    <div class="col-md-4">
+                                        <div class="post-media">
+                                            <a href="simgle.html" title="">
+                                                <img src="upload/garden_sq_05.jpg" alt="" class="img-fluid">
+                                                <div class="hovereffect"></div>
+                                            </a>
+                                        </div><!-- end media -->
+                                    </div><!-- end col -->
+
+                                    <div class="blog-meta big-meta col-md-8">
+                                        <span class="bg-aqua"><a href="category.html" title="">Indoor</a></span>
+                                        <h4><a href="simgle.html" title="">Enjoy your living room with small garden biblings</a></h4>
+                                        <p>Aenean interdum arcu blandit, vehicula magna non, placerat elit. Mauris et pharetratortor. Suspendissea sodales urna. In at augue elit. Vivamus enim nibh, maximus ac felis nec, maximus tempor odio.</p>
+                                        <small><a href="category.html" title=""><i class="fa fa-eye"></i> 2235</a></small>
+                                        <small><a href="simgle.html" title="">07 July, 2017</a></small>
+                                        <small><a href="#" title="">by Matilda</a></small>
+                                    </div><!-- end meta -->
+                                </div><!-- end blog-box -->
+
+                                <hr class="invis">
+
+                                <div class="blog-box row">
+                                    <div class="col-md-4">
+                                        <div class="post-media">
+                                            <a href="simgle.html" title="">
+                                                <img src="upload/garden_sq_06.jpg" alt="" class="img-fluid">
+                                                <div class="hovereffect"></div>
+                                            </a>
+                                        </div><!-- end media -->
+                                    </div><!-- end col -->
+
+                                    <div class="blog-meta big-meta col-md-8">
+                                        <span class="bg-aqua"><a href="category.html" title="">Indoor</a></span>
+                                        <h4><a href="simgle.html" title="">Why do you prefer small flowers at your workplace?</a></h4>
+                                        <p>Aenean interdum arcu blandit, vehicula magna non, placerat elit. Mauris et pharetratortor. Suspendissea sodales urna. In at augue elit. Vivamus enim nibh, maximus ac felis nec, maximus tempor odio.</p>
+                                        <small><a href="category.html" title=""><i class="fa fa-eye"></i> 1567</a></small>
+                                        <small><a href="simgle.html" title="">06 July, 2017</a></small>
+                                        <small><a href="#" title="">by Matilda</a></small>
+                                    </div><!-- end meta -->
+                                </div><!-- end blog-box -->
+
+                                <hr class="invis">
+
+                                <div class="blog-box row">
+                                    <div class="col-md-4">
+                                        <div class="post-media">
+                                            <a href="simgle.html" title="">
+                                                <img src="upload/garden_sq_07.jpg" alt="" class="img-fluid">
+                                                <div class="hovereffect"></div>
+                                            </a>
+                                        </div><!-- end media -->
+                                    </div><!-- end col -->
+
+                                    <div class="blog-meta big-meta col-md-8">
+                                        <span class="bg-aqua"><a href="category.html" title="">Indoor</a></span>
+                                        <h4><a href="simgle.html" title="">10 special plant species available to live in the window</a></h4>
+                                        <p>Aenean interdum arcu blandit, vehicula magna non, placerat elit. Mauris et pharetratortor. Suspendissea sodales urna. In at augue elit. Vivamus enim nibh, maximus ac felis nec, maximus tempor odio.</p>
+                                        <small><a href="category.html" title=""><i class="fa fa-eye"></i> 1451</a></small>
+                                        <small><a href="simgle.html" title="">05 July, 2017</a></small>
+                                        <small><a href="#" title="">by Matilda</a></small>
+                                    </div><!-- end meta -->
+                                </div><!-- end blog-box -->
+
+                                <hr class="invis">
+
+                                <div class="blog-box row">
+                                    <div class="col-md-4">
+                                        <div class="post-media">
+                                            <a href="simgle.html" title="">
+                                                <img src="upload/garden_sq_08.jpg" alt="" class="img-fluid">
+                                                <div class="hovereffect"></div>
+                                            </a>
+                                        </div><!-- end media -->
+                                    </div><!-- end col -->
+
+                                    <div class="blog-meta big-meta col-md-8">
+                                        <span class="bg-aqua"><a href="category.html" title="">Indoor</a></span>
+                                        <h4><a href="simgle.html" title="">Selection of high quality soil for vegetable gardening</a></h4>
+                                        <p>Aenean interdum arcu blandit, vehicula magna non, placerat elit. Mauris et pharetratortor. Suspendissea sodales urna. In at augue elit. Vivamus enim nibh, maximus ac felis nec, maximus tempor odio.</p>
+                                        <small><a href="category.html" title=""><i class="fa fa-eye"></i> 4442</a></small>
+                                        <small><a href="simgle.html" title="">01 July, 2017</a></small>
+                                        <small><a href="#" title="">by Matilda</a></small>
+                                    </div><!-- end meta -->
+                                </div><!-- end blog-box -->
+                            </div><!-- end blog-list -->
+                        </div><!-- end page-wrapper -->
+
+                        <hr class="invis">
+
+                        <div class="row">
+                            <div class="col-md-12">
+                                <nav aria-label="Page navigation">
+                                    <ul class="pagination justify-content-start">
+                                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                        <li class="page-item">
+                                            <a class="page-link" href="#">Next</a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div><!-- end col -->
+                        </div><!-- end row -->
+                    </div><!-- end col -->
+
+                    <?php include 'includes/sidebar.php'; ?>
+                </div><!-- end row -->
+            </div><!-- end container -->
+        </section>
+<?php include 'includes/footer.php'; ?>
